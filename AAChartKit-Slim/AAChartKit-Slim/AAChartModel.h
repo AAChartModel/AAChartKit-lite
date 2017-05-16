5112b4527e3315298c7194afda80433ca53682be
李照闫 <2236368544@qq.com>
2017-05-16 18:13:41 +0800

    Add  a named `AAChartView.html` file

---
+++ AAChartKit-Slim/AAChartKit-Slim/AAChartModel.h
@@ -0,0 +1,13 @@
+//
+//  AAChartModel.h
+//  AAChartKit-Slim
+//
+//  Created by An An on 2017/5/16.
+//  Copyright © 2017年 Danny boy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AAChartModel : NSObject
+
+@end
---
+++ AAChartKit-Slim/AAChartKit-Slim/AAChartModel.m
@@ -0,0 +1,13 @@
+//
+//  AAChartModel.m
+//  AAChartKit-Slim
+//
+//  Created by An An on 2017/5/16.
+//  Copyright © 2017年 Danny boy. All rights reserved.
+//
+
+#import "AAChartModel.h"
+
+@implementation AAChartModel
+
+@end
