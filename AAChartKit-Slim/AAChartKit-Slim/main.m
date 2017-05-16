c5ef527cd93171c5005f618f407e8eef9b9eb52d
李照闫 <2236368544@qq.com>
2017-05-16 17:53:03 +0800

    Create new project `AAChartKit-Slim`

---
+++ AAChartKit-Slim/AAChartKit-Slim/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AAChartKit-Slim
+//
+//  Created by Danny boy on 2017/5/16.
+//  Copyright © 2017年 Danny boy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
